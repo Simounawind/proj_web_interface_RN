253d47bef514bd40961b4eb4d5100e0a52913e71
Simounawind <1037674042@qq.com>
2024-02-28 18:35:15 +0100

    update_0228

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, File, UploadFile, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import re
-from fastapi import Request
+from typing import Optional
+import spacy
+import wikipediaapi
+import aiofiles
 
 app = FastAPI()
+
+# 加载spaCy法语模型
+nlp = spacy.load("fr_core_news_sm")
+
+wiki_wiki = wikipediaapi.Wikipedia(language='fr', user_agent="MyAppName/1.0 (myemail@example.com)")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def get_form(request: Request):
+async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/extract-urls")
-async def extract_urls(text: str = Form(...)):
-    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
-    return {"urls": urls}
+@app.post("/analyze/")
+async def analyze_text(text: str = Form(default=""), file: UploadFile = File(None)):
+    if file:
+        contents = await file.read()
+        text = contents.decode("utf-8")
+        await file.close()
+
+    if not text:
+        return {"error": "No text provided"}
+
+        
+    doc = nlp(text)
+    entities = []
+    for ent in doc.ents:
+        wiki_page = wiki_wiki.page(ent.text)
+        if wiki_page.exists():
+            entities.append({"entity": ent.text, "type": ent.label_, "url": wiki_page.fullurl})
+        else:
+            entities.append({"entity": ent.text, "type": ent.label_, "url": None})
+    return {"entities": entities}
+
