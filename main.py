9047cdac721329e3295dbff7fcd0772ffe6c07e0
刘英姿 <liuyingzi37@gmail.com>
2024-03-08 20:24:19 +0100

    ajout TingBERT

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from transformers import AutoModelForTokenClassification, AutoTokenizer, pipeline
+import torch
 from fastapi import FastAPI, Form, File, UploadFile, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 import spacy
 import wikipediaapi
 import aiofiles
-from transformers import pipeline
 
 # Créer une instance d'application FastAPI
 app = FastAPI()
@@ -21,6 +23,11 @@ nlp_spacy = spacy.load("fr_core_news_sm")
 # Initialiser les modèles de reconnaissance d'entités nommées (NER) de Transformers
 nlp_bert_base_ner = pipeline("ner", model="dslim/bert-base-NER")
 nlp_wikineural_ner = pipeline("ner", model="Babelscape/wikineural-multilingual-ner")
+
+# Initialiser le modèle TinyBERT finetuned pour la NER
+tokenizer_custom = AutoTokenizer.from_pretrained("Tinybert-finetuned-ner")
+model_custom = AutoModelForTokenClassification.from_pretrained("Tinybert-finetuned-ner")
+nlp_custom = pipeline("ner", model=model_custom, tokenizer=tokenizer_custom)
 
 # Initialiser WikipediaAPI avec un agent utilisateur personnalisé pour les requêtes
 wiki_wiki = wikipediaapi.Wikipedia(language='fr', user_agent="MyAppName/1.0 (myemail@example.com)")
@@ -82,12 +89,28 @@ async def analyze_text(text: str = Form(default=""), file: UploadFile = File(Non
 
         print("Entités traitées par Wikineural :", entities_wikineural_ner)
 
+        # utiliser le modèle TinyBERT finetuned pour la NER
+        results_custom = nlp_custom(text)
+
+        entities_custom = []
+        for result in results_custom:
+            entity_text = text[result['start']:result['end']]
+            score = result.get('score')
+            if score is not None:  # faire en sorte que le score existe
+                score = float(score)  # convertir le score en float standard
+            entities_custom.append({"entity": entity_text, "type": result['entity'], "score": score})
+
+        # retourner la réponse JSON
+        # return JSONResponse(content={"entities_custom_model": entities_custom})
+
         # Fusionner les résultats et les retourner
         return {
             "entities_spacy": entities_spacy,
             "entities_bert_base_ner": entities_bert_base_ner,
-            "entities_wikineural_ner": entities_wikineural_ner
+            "entities_wikineural_ner": entities_wikineural_ner,
+            "entities_custom_model": entities_custom
         }
     except Exception as e:
         print(f"Une erreur est survenue : {e}")
         return {"error": "Erreur interne du serveur"}, 500
+
